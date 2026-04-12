c587869d96e1964742d65858ebebe5c6ea290ccd
FedericoVassallo <federico.vassallo2003@gmail.com>
2026-04-12 18:39:59 +0200

    update build script

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-g++ -std=c++17 -O2 -o scheduler src/main.cpp src/data_structures.cpp src/dependency.cpp src/io_handler.cpp src/scheduler.cpp src/register_allocation.cpp -I.
+g++ -std=c++17 -O2 -o scheduler \
+    src/main.cpp \
+    src/data_structures.cpp \
+    src/dependency.cpp \
+    src/io_handler.cpp \
+    src/scheduler.cpp \
+    src/register_allocation.cpp \
+    -Iinclude
