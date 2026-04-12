14893959528afb16796b2ce37e313d765a071896
FedericoVassallo <federico.vassallo2003@gmail.com>
2026-04-12 15:33:28 +0200

    created the register_allocation.hpp

---
+++ src/register_allocation.cpp
@@ -0,0 +1,7 @@
+// for this hw we always have the assumption of having enough registers.
+
+#include "register_allocation.hpp"
+#include "scheduler.hpp"
+#include "data_structures.hpp"
+#include <stdexcept>
+
