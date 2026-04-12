76bd5d43d403c6590025fef31ade2117538d98f1
FedericoVassallo <federico.vassallo2003@gmail.com>
2026-04-12 18:17:14 +0200

    modified the build.sh and run.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-make clean && make
+g++ -std=c++17 -O2 -o scheduler src/main.cpp src/data_structures.cpp src/dependency.cpp src/io_handler.cpp src/scheduler.cpp src/register_allocation.cpp -I.
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./vliw_scheduler "$1" "$2" "$3"
+./scheduler "$1" "$2" "$3"
